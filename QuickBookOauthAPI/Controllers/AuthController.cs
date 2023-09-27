1be1aec9b1f9358162247514ab6a1d2b862be01a
Daler <dalerjon.olimov@silkroadprofessionals.com>
2023-09-27 13:30:15 +0500

    delete comments

--- QuickBookOauthAPI/Controllers/AuthController.cs
+++ QuickBookOauthAPI/Controllers/AuthController.cs
@@ -11,12 +11,12 @@ public class AuthController : ControllerBase
     private const string ClientId = "AB982kbi6DYVFZK8Ea4oaQDkss3SJmUSYjR7OJMDa2zIOiRBtS";
     private const string ClientSecret = "3PyFAlJ3bDPG0uGuHWqvYAUFosOjVPsv1qE0dZeZ";
     private const string Environment = "sandbox";
-    private const string redirectUrl = "https://localhost:44306/receiver";
+    private const string RedirectUri = "https://localhost:44306/receiver";
     private readonly OAuth2Client _oAuth2Client;
     
     public AuthController()
     {
-        _oAuth2Client = new OAuth2Client(ClientId, ClientSecret, redirectUrl, Environment);
+        _oAuth2Client = new OAuth2Client(ClientId, ClientSecret, RedirectUri, Environment);
     }
     
     [HttpPost]
