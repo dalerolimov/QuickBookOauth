efc5cff8ccb916b59dd9821aae7e82c0952f9adf
Daler <dalerjon.olimov@silkroadprofessionals.com>
2023-09-27 10:20:58 +0500

    any changes

---
+++ QuickBookOauthAPI/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Intuit.Ipp.OAuth2PlatformClient;
+using Microsoft.AspNetCore.Mvc;
+using QuickBookOauth.Entities;
+
+namespace QuickBookOauthAPI.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private const string ClientId = "AB982kbi6DYVFZK8Ea4oaQDkss3SJmUSYjR7OJMDa2zIOiRBtS";
+    private const string ClientSecret = "3PyFAlJ3bDPG0uGuHWqvYAUFosOjVPsv1qE0dZeZ";
+    private const string Environment = "sandbox";
+    private const string redirectUrl = "https://localhost:44306/receiver";
+    private readonly OAuth2Client _oAuth2Client;
+    
+    public AuthController()
+    {
+        _oAuth2Client = new OAuth2Client(ClientId, ClientSecret, redirectUrl, Environment);
+    }
+    
+    [HttpPost]
+    public async Task<string> AuthorizationCode(AuthClaims auth)
+    {
+        var token = await _oAuth2Client.GetBearerTokenAsync(auth.Code); 
+        
+        return token.AccessToken;
+    }
+}
---
+++ QuickBookOauthAPI/Entities/AuthClaims.cs
@@ -0,0 +1,8 @@
+﻿namespace QuickBookOauth.Entities;
+
+public class AuthClaims
+{
+    public string Code { get; set; }
+    public string State { get; set; }
+    public string RealmId { get; set; }
+}
